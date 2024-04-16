ce881a858dcf06252d9d2d90d2f22512c423e785
samscarlett <samscarlett07@gmail.com>
2024-04-16 14:45:50 -0400

    new edits

---
+++ script.js
@@ -0,0 +1,19 @@
+
+//call all my shit
+const dropDownOptions = function() {
+
+  let cMajor = document.getElementById("C Major");
+  let fMajor = document.getElementById("F Major");
+  let bFlatMajor = document.getElementById("Bb Major");
+  let eFlatMajor = document.getElementById("Eb Major");
+  let aFlatMajor = document.getElementById("Ab Major");
+  let dFlatMajor = document.getElementById("Db Major");
+  let gFlatFSharpMajor = document.getElementById("Gb/F# Major");
+  let gMajor = document.getElementById("G Major");
+  let dMajor = document.getElementById("D Major");
+  let aMajor = document.getElementById("A Major");
+  let eMajor = document.getElementById("E Major");
+  let bMajor = document.getElementById("B Major");
+
+}
+  
