d9ee94b8f771118a21b5c8eb4bebc2412b14e61b
samscarlett <samscarlett07@gmail.com>
2024-04-23 18:12:46 -0400

    new edits for development 2

--- MusicTools.js
+++ MusicTools.js
@@ -1,20 +1,5 @@
 await WebMidi.enable();
 
-const MusicTools = {
-    standardPitch: 440, 
-/**
- * Converts MIDI pitch to the corresponding frequency in Hertz (A440)
- * @param {number} midiPitch Pitch in MIDI Number
- * @returns {number}  Frequency in Hertz
- * @example
- * 
- */
-    midiPitchToFrequency: function (midiPitch) {
-        return this.standardPitch * Math.pow(2, (midiPitch - 69) / 12);
-      },
-
-    };
-
 //enable WebMIDI API
 
   let myInput = WebMidi.inputs[0];
@@ -33,58 +18,50 @@ WebMidi.outputs.forEach(function (output, num) {
     dropOuts.innerHTML += `<option value=${num}>${output.name}</option>`;
   });
 
-  //define MIDI processing function
+//read option n shit
+WebMidi.inputs.forEach(function (input, num) {
+  dropIns.innerHTML += `<option value=${num}>${input.name}</option`;
+});
+
+WebMidi.inputs.forEach(function (output, num) {
+  dropOuts.innerHTML += `<option value=${num}>${output.name}</option`;
+});
+
+
+//note reader n shit
 const midiProcess = function (midiNoteInput) {
-  
-    let pitch = midiNoteInput.note.number;
-    let velocity = midiNoteInput.note.rawAttack;
-  
-    let midiNoteOutput = new Note(pitch, { rawAttack: velocity });
-    return midiNoteOutput;
-  };
 
-// Add an event listener for the 'change' event on the input devices dropdown.
-// This allows the script to react when the user selects a different MIDI input device.
+  let pitch = midiNoteInput.note.number;
+  let velocity = midiNoteInput.note.rawAttack;
+
+  let midiNoteOutput = new Note(pitch, { rawAttack: velocity });
+  return midiNoteOutput;
+};
+
 dropIns.addEventListener("change", function () {
-    // Before changing the input device, remove any existing event listeners
-    // to prevent them from being called after the device has been changed.
-    if (myInput.hasListener("noteon")) {
-      myInput.removeListener("noteon");
-    }
-    if (myInput.hasListener("noteoff")) {
+  if (myInput.hasListener("noteon")) {
+  myInput.removeListener("noteon");
+  }
+  if(myInput.hasListener("noteoff")) {
       myInput.removeListener("noteoff");
-    }
-  
-    // Change the input device based on the user's selection in the dropdown.
-    myInput = WebMidi.inputs[dropIns.value];
-    // myOutput = WebMidi.outputs[dropOuts.value];
-  
-    
-  
-    // After changing the input device, add new listeners for 'noteon' and 'noteoff' events.
-    // These listeners will handle MIDI note on (key press) and note off (key release) messages.
-    myInput.addListener("noteon", function (someMIDI) {
-      // When a note on event is received, send a note on message to the output device.
-      // This can trigger a sound or action on the MIDI output device.
-      myOutput.sendNoteOn(midiProcess(someMIDI));
-    });
-  
-    myInput.addListener("noteoff", function (someMIDI) {
-      // Similarly, when a note off event is received, send a note off message to the output device.
-      // This signals the end of a note being played.
-  
-      myOutput.sendNoteOff(midiProcess(someMIDI));
-    });
-  });
-  
-  // Add an event listener for the 'change' event on the output devices dropdown.
-  // This allows the script to react when the user selects a different MIDI output device.
-  dropOuts.addEventListener("change", function () {
-    // Change the output device based on the user's selection in the dropdown.
-    // The '.channels[1]' specifies that the script should use the first channel of the selected output device.
-    // MIDI channels are often used to separate messages for different instruments or sounds.
-    myOutput = WebMidi.outputs[dropOuts.value].channels[1];
-  });
+  }
 
-    export default MusicTools;
+  myInput = WebMidi.inputs[dropIns.value];
+
+  myInput.addListener("noteon", function (someMIDI) {
+      myOutput.sendNoteOn(midiProcess(someMIDI));
+});
+
+  myInput.addListener("noteoff", function (someMIDI) {
+      myOutput.sendNoteOff(midiProcess(someMIDI));
+  });
+});
+
+
+
+dropOuts.addEventListener("change", function () {
+  myOutput = WebMidi.outputs [dropOuts.value].channels[1];
+});
+
+export default MusicTools;
 
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+await WebMidi.enable();
+import MusicTools from "./MusicTools.js";
 
-//call all my shit
+
+//call all my scales n shit
 const dropDownOptions = function() {
 
   let cMajor = document.getElementById("C Major");
@@ -15,5 +18,17 @@ const dropDownOptions = function() {
   let eMajor = document.getElementById("E Major");
   let bMajor = document.getElementById("B Major");
 
+};
+
+//make a c major scale 
+let cMajorScale = [24, 26, 28, 29, 31, 33, 35, 36, 38, 40, 41, 43, 45, 47, 48, 50, 52, 53, 55, 57, 59, 60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83, 84, 86, 88, 89, 91, 93, 95, 96]
+cMajor = cMajorScale;
+
+if (dropDownOptions = cMajor) {
+  
 }
+
+
+
+
   
